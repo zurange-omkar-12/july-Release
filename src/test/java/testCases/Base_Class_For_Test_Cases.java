e3e13b10f562a968cc6e740952904fa7c1fdd51c
zurange_omkar <zurangeomkar7@gmail.com>
2024-07-07 01:36:10 +0530

    0J.07.003 release with config.properties file

--- src/test/java/testCases/Base_Class_For_Test_Cases.java
+++ src/test/java/testCases/Base_Class_For_Test_Cases.java
@@ -5,7 +5,10 @@ import pageObjects.*;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,21 +19,38 @@ public class Base_Class_For_Test_Cases {
 	WebDriver driver_Obj;
 	WebDriverWait wait_Obj;
 	
+	Properties properties_Obj;
+	
+	FileReader fileReader_Obj;
+			
 	SoftAssert softAssert_Obj;
 	
 	orangeHRM_Login_Page HRM_Login_Page;
+	
 	orangeHRM_Dashboard_Page HRM_Dashboard_Page;
 	
+	
   @BeforeMethod
-  public void beforeClass() {
+  public void beforeClass() throws IOException {
+	  
+	  properties_Obj=new Properties();
+	  
+	  fileReader_Obj = new FileReader("./src/test/resources/config.properties");
+	  
+	  properties_Obj.load(fileReader_Obj);
 	  
 	  driver_Obj = new ChromeDriver();
 	  wait_Obj = new WebDriverWait(driver_Obj,Duration.ofSeconds(10));
 	  softAssert_Obj = new SoftAssert();
 	  
 	  driver_Obj.manage().window().maximize();
-	  driver_Obj.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	  driver_Obj.get(properties_Obj.getProperty("appURL"));
+  
+	  
+  
   }
+  
+  
 
   @AfterMethod
   public void afterClass() throws InterruptedException {
